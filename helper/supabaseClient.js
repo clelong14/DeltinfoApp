8eb596baa3b7697c9ffac6026a61a4b8a1f7ae8c
clelong14 <colynne.lelong.pro@gmail.com>
2025-05-28 11:40:45 +0200

    Update supabaseClient.js

--- helper/supabaseClient.js
+++ helper/supabaseClient.js
@@ -1,9 +1,12 @@
 import { RealtimeClient } from '@supabase/realtime-js';
 
-// Utiliser le WebSocket natif du navigateur
-const realtime = new RealtimeClient('wss://https://mocnbrmswtszbwrhiznk.supabase.co/realtime/v1', {
-  params: {
-    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vY25icm1zd3RzemJ3cmhpem5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MTAxMTEsImV4cCI6MjA2MzM4NjExMX0.V3n-mmjs6tVtokfCQSw_dfSFh8eDQX8rQ1ypgqGLqDQ',
-  },
-  WebSocket: window.WebSocket,  // <-- Force le WebSocket natif
-});
+export const createRealtimeClient = () => {
+  if (typeof window === 'undefined') return null;
+
+  return new RealtimeClient('wss://mocnbrmswtszbwrhiznk.supabase.co/realtime/v1', {
+    params: {
+      apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vY25icm1zd3RzemJ3cmhpem5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MTAxMTEsImV4cCI6MjA2MzM4NjExMX0.V3n-mmjs6tVtokfCQSw_dfSFh8eDQX8rQ1ypgqGLqDQ',
+    },
+    WebSocket: window.WebSocket,
+  });
+};
