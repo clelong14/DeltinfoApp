4a2730ace419a9c1cf7c80201f7d816f6553d16a
clelong14 <colynne.lelong.pro@gmail.com>
2025-05-28 11:38:00 +0200

    Update supabaseClient.js

--- helper/supabaseClient.js
+++ helper/supabaseClient.js
@@ -1,21 +1,9 @@
-import 'react-native-url-polyfill/auto'; // obligatoire
-import { createClient } from '@supabase/supabase-js';
+import { RealtimeClient } from '@supabase/realtime-js';
 
-// Parfois, tu dois forcer fetch global (mais tu l'as déjà fait, c'est bien)
-// Et ajouter d'autres polyfills si nécessaire.
-
-const supabaseUrl = "https://mocnbrmswtszbwrhiznk.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vY25icm1zd3RzemJ3cmhpem5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MTAxMTEsImV4cCI6MjA2MzM4NjExMX0.V3n-mmjs6tVtokfCQSw_dfSFh8eDQX8rQ1ypgqGLqDQ";
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: false,
+// Utiliser le WebSocket natif du navigateur
+const realtime = new RealtimeClient('wss://https://mocnbrmswtszbwrhiznk.supabase.co/realtime/v1', {
+  params: {
+    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vY25icm1zd3RzemJ3cmhpem5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MTAxMTEsImV4cCI6MjA2MzM4NjExMX0.V3n-mmjs6tVtokfCQSw_dfSFh8eDQX8rQ1ypgqGLqDQ',
   },
-  global: {
-    fetch: fetch, // important
-  },
+  WebSocket: window.WebSocket,  // <-- Force le WebSocket natif
 });
-
-export default supabase;
